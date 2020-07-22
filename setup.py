769ff8c200088f945ba8dcb68d120dab3d10d1e3
gradybooch <egrady@booch.com>
2020-07-21 17:43:55 -1000

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open('README.md', 'r') as readMe:
+    long_description = readMe.read()
+
+setuptools.setup(
+
+    name = 'self_concepts'
+    version = '1.0'
+    author = 'Grady Booch'
+    author_email = 'egrady@booch.com')
+    description = 'Self''s foundational abstractions'
+
+)

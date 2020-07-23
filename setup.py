243f7cfff13bb2113dae103d4254c494a8274903
gradybooch <egrady@booch.com>
2020-07-23 09:28:35 -1000

    Continuing development.

--- setup.py
+++ setup.py
@@ -5,10 +5,22 @@ with open('README.md', 'r') as readMe:
 
 setuptools.setup(
 
-    name = 'self_concepts'
-    version = '1.0'
-    author = 'Grady Booch'
-    author_email = 'egrady@booch.com')
-    description = 'Self''s foundational abstractions'
+    name = 'self_concepts',
+    version = '1.0',
+    author = 'Grady Booch',
+    author_email = 'egrady@booch.com',
+    description = 'Self''s foundational abstractions',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    keywords = 'self agi neuro-symbolic',
+    url = 'https://github.com/booch-self/self-concepts',
+    packages = ['self_concepts'],
+    package_dir = ['self_concepts': 'source/python'],
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires = '>=3.8'
 
 )
